cb3360b8280e307ce30bda859e0ef059cfcb2c2c
krishnapurbia <138788042+krishnapurbia@users.noreply.github.com>
2025-08-10 01:28:52 +0530

    Create setorderedtree.cpp
    setorderedredblacktree

---
+++ setorderedtree.cpp
@@ -0,0 +1,8 @@
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using Tree = tree <T, null_type,less<T>,rb_tree_tag, tree_order_statistics_node_update>;
+
+
+// // find_by_order
+// // order_of_key
